26b24598535b0ffb3c78feb71bf00360cf0f5aa0
Rudi <rvlglobe@gmail.com>
2017-04-25 20:31:16 +0100

    initial core of web page

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+// connect = require('gulp-connect'),
+connect = require('gulp-connect'),
+sass = require('gulp-sass');
+
+gulp.task('connect', function() {
+	connect.server({
+		root: 'src',
+		livereload: true
+	});
+});
+
+gulp.task('reload', function () {
+	gulp.src('./src/*.html')
+	.pipe(connect.reload());
+});
+
+gulp.task('sass', function(){
+  return gulp.src('./src/assets/sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./src/assets/css'));
+});
+
+gulp.task('watch', function () {
+	gulp.watch(['./src/*.html'], ['reload']);
+	gulp.watch(['./src/js/*.js'], ['reload']);
+	gulp.watch('./src/assets/sass/*.scss', ['sass', 'reload']);
+});
+
+gulp.task('default', ['connect', 'watch', 'sass']);
---
+++ src/js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+	$('html').niceScroll({scrollspeed:5, mousescrollstep:50, cursorwidth:10, cursorborderradius: 5, cursorcolor: '#800000'});
+	
+});
