3a372f33dab78b5861bbb061f75ff839b1a89a30
Rudi <rvlglobe@gmail.com>
2017-04-27 09:24:32 +0100

    add images and animate library

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,38 @@
 $(document).ready(function(){
 
-	$('html').niceScroll({scrollspeed:5, mousescrollstep:50, cursorwidth:10, cursorborderradius: 5, cursorcolor: '#800000'});
+	$('html').niceScroll({scrollspeed:5, mousescrollstep:250, cursorwidth:10, cursorborderradius: 5, cursorcolor: '#800000'});
 
 	/* toggle sidebar */
 	$('#bars').on('click', function(){
 
 		if($('.fa-bars').length === 1){
-			$('.fa-bars').removeClass('fa-bars').addClass('fa-times');		
+			$('#sidebar-nav')
+			.removeClass('slideOutUp')
+			.addClass('slideInRight');
+			$('.fa-bars')
+			.removeClass('fa-bars')
+			.addClass('fa-times')
+			.addClass('button--m-r-12');		
 		}
 
 		else if($('.fa-times').length === 1){
-			$('.fa-times').removeClass('fa-times').addClass('fa-bars');
+			$('#sidebar-nav')
+			.removeClass('slideInRight')
+			.addClass('slideOutUp');
+			$('.fa-times')
+			.removeClass('fa-times')
+			.removeClass('button--m-r-12')
+			.addClass('fa-bars');
 		}
 
 		else if($('.fa-times').length === 2){
-			$('#bars').find('.fa-times').removeClass('fa-times').addClass('fa-bars');
+			$('#bars')
+			.find('.fa-times')
+			.removeClass('fa-times')
+			.removeClass('button--m-r-12')
+			.removeClass('slideInRight')
+			.addClass('fa-bars')
+			.addClass('slideOutUp');
 		}
 		
 	});
@@ -23,14 +41,32 @@ $(document).ready(function(){
 	$('#social').on('click', function(){
 
 		if($('.fa-share-alt').length === 1){
-			$('.fa-share-alt').removeClass('fa-share-alt').addClass('fa-times');		
+			$('#sidebar-social')
+			.removeClass('slideOutUp')
+			.addClass('slideInLeft');
+			$('.fa-share-alt')
+			.removeClass('fa-share-alt')
+			.addClass('fa-times')
+			.addClass('button--m-l-12');
 		}
 
 		else if($('.fa-times').length === 1){
-			$('.fa-times').removeClass('fa-times').addClass('fa-share-alt');
+			$('#sidebar-social')
+			.removeClass('slideInLeft')
+			.addClass('slideOutUp');
+			$('.fa-times')
+			.removeClass('fa-times')
+			.removeClass('button--m-l-12')
+			.addClass('fa-share-alt');
 		}
 		else if($('.fa-times').length === 2){
-			$('#social').find('.fa-times').removeClass('fa-times').addClass('fa-share-alt');
+			$('#social')
+			.find('.fa-times')
+			.removeClass('fa-times')
+			.removeClass('button--m-l-12')
+			.removeClass('slideInLeft')
+			.addClass('fa-share-alt')
+			.addClass('slideOutUp');
 		}		
 		
 	});	
