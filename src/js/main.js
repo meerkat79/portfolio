edd8814756914b5bb045636b519ddbbb46bf535e
Rudi <rvlglobe@gmail.com>
2017-04-27 17:10:13 +0100

    build vortex tiles

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,39 @@
 @Note: Main JS file. Note custom plugins can be found in plugins.js
 */
 
-//niceScroll config params
-$(document).ready(function(){
-	$('html').niceScroll({scrollspeed:5, mousescrollstep:100, cursorwidth:10, cursorborderradius: 5, cursorcolor: '#800000'});	
-});
 
-//vortex congif params
-$(window).load(function() {
-	$('#vortex').vortex({
-		initialPosition: 270,
-		speed: 250,
-		deepFactor: 0.3
+$(document).ready(function(){
+
+	//niceScroll config params
+	$('html').niceScroll({scrollspeed:5, mousescrollstep:100, cursorwidth:10, cursorborderradius: 5, cursorcolor: '#800000'});	
+
+	//vortex congif params
+	$(window).load(function() {
+		$('#vortex').vortex({
+			initialPosition: 0,
+			speed: 250,
+			deepFactor: 0.3
+		});
 	});
+
+	//some vortex controls
+
+	var vortexBtn = $('#vortexBtn');
+	var vortex = $('#vortex');
+
+	$(vortexBtn).on('click', function(){
+		console.log('click');
+		if(vortexBtn.text() === 'Pause'){
+			console.log('pause');			
+			$(vortex).data('vortex').stop();
+			$(vortexBtn).text('Play');
+		}
+		else if(vortexBtn.text() === 'Play'){
+			console.log('play');
+			$(vortex).data('vortex').start();
+			$(vortexBtn).text('Pause');
+		}
+		
+	});
+
 });
