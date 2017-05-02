1a910c3dbfff68fc0fa49bf41946104f481cd80b
Rudi <rvlglobe@gmail.com>
2017-05-02 14:08:05 +0100

    add a from object in submit response and small fix

--- src/submit.php
+++ src/submit.php
@@ -8,9 +8,10 @@ $subject = $_POST['subject'];
 $message = $_POST['message'];
 $to = 'rvlglobe@gmail.com';
 $subject = 'Message from irudi.me';
+$headers = 'From: webmaster@irudi.me' . '\r\n';
 
-mail($to, $subject, $message, 'From: ' . $name);
-echo 'Your message has been sent';
+mail($to, $subject, $message, 'From: ' . $name, $headers);
+echo 'Thank you for your message. I will aim to get back in touch as soon as possible.';
 
 
 
